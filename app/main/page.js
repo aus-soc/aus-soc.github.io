05995b2455861f10d96df988e43394a0f203078e
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2024-08-06 22:11:39 +0400

    updated Nav

--- app/main/page.js
+++ app/main/page.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import Navbar from '@/components/navbar'
+import { Box, AppBar } from '@mui/material'
 
 function main() {
   return (
-    <Navbar />
+    <Box width="100vw" height="100vh" 
+    display="flex"
+    flexDirection={"column"}
+    justifyContent="space-between"
+    alignItems="start"
+    gap={2} className="bg-black">
+      <Navbar />
+    </Box>
   )
 }
 
--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,20 @@
+'use client'
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { Box, AppBar, Button, Toolbar, Tooltip, IconButton, Typography, MenuIcon, Container, Avatar, MenuItem } from '@mui/material'
+import AdbIcon from '@mui/icons-material/adb'
+import Menu from '@mui/icons-material/Menu'
+import App from 'next/app'
 
 function navbar() {
-  return (
-    <div>navbar</div>
-  )
+    return (
+        <AppBar position="static"   className='bg-transparent flex justify-center flex-row gap-12 place-content-evenly py-8'>
+            <a href='#'><Typography className='font-bold'>Upcoming Events</Typography></a>
+            <a href='#'><Typography className='font-bold'>Previous Events</Typography></a>
+            <a href='#'><Typography className='font-bold'>FAQ</Typography></a>
+            <a href='#'><Typography className='font-bold'>Members</Typography></a>
+        </AppBar>
+    );
 }
 
 export default navbar
