b1d2d7d10cfb13bc63a7f3853a93324a9b1da19d
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2024-08-11 11:35:50 +0400

    updated

--- app/main/page.js
+++ app/main/page.js
@@ -3,7 +3,7 @@ import Navbar from '@/components/navbar'
 import Hero from '@/components/hero'
 import Upcoming from '@/components/upcoming_events'
 import About from '@/components/about'
-import Members from '@/components/members'
+// import Members from '@/components/members'
 import { Box, Card} from '@mui/material'
 
 
@@ -16,7 +16,7 @@ function main() {
       <Hero />
       <Upcoming />
       <About />
-      <Members />
+      {/* <Members /> */}
     </Box>
   )
 }
--- components/members.js
+++ components/members.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Box, Typography } from '@mui/material'
-import BubbleChart from '@/components/bubblechart'
 
 function Members() {
   return (
     <Box width={"100vw"} className="bg-white flex flex-col gap-10 justify-center items-center py-10">
         <Typography variant='h1' className='font-bold text-6xl text-gray-900'>Members</Typography>
-        <BubbleChart/> 
     </Box>
   )
 }
